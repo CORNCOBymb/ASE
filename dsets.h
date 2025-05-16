13b13ab0ba7c8c8cd40250af2b9ffb1509472df0
yanzheng23 <qwe@LAPTOP-LMPJH6AD>
2025-05-16 10:11:56 +0800

    123

--- dsets.h
+++ dsets.h
@@ -1,1 +1,24 @@
 /* Your code here! */
+#ifndef _DSETS_H_
+#define _DSETS_H_
+
+#include <vector>
+
+using std::vector;
+
+class DisjointSets
+{
+public:
+    void addelements(int num);
+
+    int find(int elem);
+
+    void setunion(int a, int b);
+
+    int size(int elem);
+
+private:
+    vector<int> _elems;
+};
+
+#endif
